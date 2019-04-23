d06b50b33b26eda781ec20471cfd0d7d72e8b219
jkaddu <jkaddu@yahoo.com>
2019-04-23 15:05:55 -0700

    refactored, debugging

--- data/seeds/seeds-router.js
+++ data/seeds/seeds-router.js
@@ -1,42 +1,51 @@
 const express = require('express');
 
-const Seeds = require('./post.js');
+const Seeds = require('./db.js');
 
 const router = express.Router();
 
 router.get('/api/posts', (req, res) => {
-   db 
-   .find()
-   .then(posts => {
-       res.json(posts)
-   })
-   .catch(err => {
-       res.status(500).json({ error: "The posts information could not be retrieved." })
-   });
+    try {
+        const seeds = await Seeds.find(req.query);
+        res.status(200).json(hubs);
+      } catch (error) {
+        // log error to database
+        console.log(error);
+        res.status(500).json({
+            error: "The posts information could not be retrieved.",
+        });
+      }
 });
 
 router.get('/api/posts/:id', (req, res) => {
-    db 
-    .findById(id)
-    .then(posts => {
-        res.json(posts)
-    })
-    .catch(err => {
-        res.status(404).json({ message: "The post with the specified ID does not exist." })
-    });
+    try {
+        const seed = await Seeds.findById(req.params.id);
+    
+        if (seed) {
+          res.status(200).json(seed);
+        } else {
+          res.status(404).json({ message: "The post with the specified ID does not exist." });
+        }
+      } catch (error) {
+        // log error to database
+        console.log(error);
+        res.status(500).json({
+            error: "The post information could not be retrieved.",
+        });
+      }
  });
 
 router.post('/api/posts', (req, res) => {
-     const newPost = req.body;
-     console.log('request body:', newPost);
-     db
-     .inset(newPost)
-     .then(posts => {
-         res.status(201).json(posts)
-     })
-     .catch(err => {
-         res.status(500).json({ error: "There was an error while saving the post to the database"  })
-     });
+    try {
+        const seed = await Seeds.add(req.body);
+        res.status(201).json(seed);
+      } catch (error) {
+        // log error to database
+        console.log(error);
+        res.status(500).json({
+            error: "There was an error while saving the post to the database",
+        });
+      }
 });
  
 router.delete('./api/posts/:id', (req, res) => {
@@ -56,23 +65,20 @@ router.delete('./api/posts/:id', (req, res) => {
 });
 
 router.put('/api/post/:id', (req, res) => {
-     const {id} = req.params;
-     const post = req.body;
-
-     if(!post.title || !post.contents)
-        return res.status(400).json({ errorMessage: "Please provide title and contents for the post."  })
-        .end();
-
-    db
-    .update(id, post)
-    .then(post => {
-        if(!post)
-            return res.status(404).json({ message: "The post with the specified ID does not exist." });
-            return res.status(200).json(psot);
-    })
-    .catch(err => {
-        res.status(500).json({ error: "The post information could not be modified." })
-    });
+    try {
+        const seed = await Seeds.update(req.params.id, req.body);
+        if (seed) {
+            res.status(200).json(hub);
+        } else {
+            res.status(404).json({ message: "The post with the specified ID does not exist." });
+        }
+    } catch (error) {
+        // log error to database
+        console.log(error);
+        res.status(500).json({
+            error: "The post information could not be modified.",
+        });
+    }
 });
 
 
